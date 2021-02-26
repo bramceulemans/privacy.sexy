e82cce6835be8c170b5e6bea03dcdb2ef7d6a366
undergroundwires <undergroundwires@users.noreply.github.com>
2021-02-26 20:55:38 +0100

    refactor all modals to use same parent component

---
+++ src/presentation/shared/Dialog.vue
@@ -0,0 +1,53 @@
+<template>
+    <modal
+        :name="name"
+        :scrollable="true"
+        :adaptive="true"
+        height="auto">
+    <div class="dialog">
+      <div class="dialog__content">
+          <slot></slot>
+      </div>
+      <div class="dialog__close-button">
+        <font-awesome-icon :icon="['fas', 'times']"  @click="$modal.hide(name)"/>
+      </div>
+    </div>
+  </modal>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Dialog extends Vue {
+  @Prop() public name: string;
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/presentation/styles/colors.scss";
+@import "@/presentation/styles/fonts.scss";
+
+.dialog {
+  font-family: $normal-font;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+
+  &__content {
+    width: 100%;
+    margin: 5%;
+  }
+
+  &__close-button {
+    width: auto;
+    font-size: 1.5em;
+    margin-right:0.25em;
+    align-self: flex-start;
+    cursor: pointer;
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+}
+</style>
